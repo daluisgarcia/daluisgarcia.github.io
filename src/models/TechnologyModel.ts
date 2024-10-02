5802af9af920a2c6c1ad64e12fa2afb811795d65
daluisgarcia <daluisgarcia@gmail.com>
2024-10-02 14:27:57 -0400

    Techonology model icon_name is now required

--- src/models/TechnologyModel.ts
+++ src/models/TechnologyModel.ts
@@ -2,7 +2,7 @@ export default interface Technology {
     id: number;
     name: string;
     time_of_experience?: string;
-    icon_name?: string;
+    icon_name: string;
     base_tech?: Technology | null;
     is_selected?: boolean;
 }
